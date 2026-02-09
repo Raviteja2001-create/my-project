b6e35b77f95269f49d7e89abbabfa89a683da2bf
Ravi <ravitejabarli541@gmail.com>
2026-02-09 13:15:03 +0000

    updated terraform file

---
+++ Modules/Webserver/variables.tf
@@ -0,0 +1,20 @@
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "public_key_location" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "vpc_id" {}
+
+
+variable "image_name" {}
+
+
+  
+
+
+
+  
+
+
+
+  
--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-  
-
 
 resource "aws_vpc" "myapp_vpc" {
   cidr_block = var.vpc_cidr_block
@@ -20,108 +7,30 @@ resource "aws_vpc" "myapp_vpc" {
   }
 }
 
-resource "aws_subnet" "myapp_subnet_1" {
-  vpc_id            = aws_vpc.myapp_vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
+module "my_app_subnet" {
+source = "./Modules/Subnet"
+subnet_cidr_block = var.subnet_cidr_block
+avail_zone = var.avail_zone
+env_prefix = var.env_prefix
+vpc_id = aws_vpc.myapp_vpc.id
+default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
 }
 
-resource "aws_internet_gateway" "myapp_igw" {
-  vpc_id = aws_vpc.myapp_vpc.id
+module "my_instance" {
+source = "./Modules/Webserver"
+env_prefix = var.env_prefix
+vpc_id = aws_vpc.myapp_vpc.id
+ my_ip = var.my_ip
+public_key_location = var.public_key_location
+instance_type = var.instance_type
+subnet_id = module.my_app_subnet.subnet_id
+image_name = var.image_name
 
-  tags = {
-    Name = "${var.env_prefix}-igw"
-  }
+
+  
 }
 
-resource "aws_default_route_table" "main_rtb" {
-  default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp_igw.id
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-main-rtb"
-  }
+output "subnet_id" {
+  value = module.my_app_subnet.subnet_id
 }
 
-resource "aws_security_group" "myapp_sg" {
-  name   = "${var.env_prefix}-sg"
-  vpc_id = aws_vpc.myapp_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-sg"
-  }
-}
-
-data "aws_ami" "latest_amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-output "aws_ami_id" {
-    value = data.aws_ami.latest_amazon_linux_2.id
-}
-
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
-    public_key = file(var.public_key_location)
-
-}
-
-
-
-resource "aws_instance" "myapp_server" {
-  ami           = data.aws_ami.latest_amazon_linux_2.id
-  instance_type = var.instance_type
-
-  subnet_id              = aws_subnet.myapp_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.myapp_sg.id]
-
-  associate_public_ip_address = true
-  key_name                      = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh")
-
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
-}
